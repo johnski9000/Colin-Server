67bffbb8c1d10c9097535bac96d37322542dbf77
Janusz Wozniak <januszwozniak.bth@gmail.com>
2023-04-01 19:28:11 +0100

    vercel config

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
+const express = require('express');
+const nodemailer = require('nodemailer');
 const cors = require("cors");
-const allowCors = require("./allowCors")
 
 const app = express();
-const port = process.env.PORT || 3001;;
-
+const port = process.env.PORT || 3001;
 app.use(cors());
+app.options('*', cors());
 
-
-function sendEmail(props) {
-    return new Promise((resolve, reject) => {
-        var transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: "jwdigitalltd@gmail.com",
-                pass: "fidvjwawgbyssidd"
-            },
-        });
-        const mail_configs = {
-            from: "jwdigitalltd@gmail.com",
-            to: "januszwozniak.bth@gmail.com",
-            subject: "Testing Koding 101 email",
-            text: "Just checking if this email will be sent"
-        };
-        transporter.sendMail(mail_configs, function(error, info) {
-            if (error) {
-                console.log(error)
-                return reject({ message: "an error has occured"})
-            } 
-            return resolve ({message: "email sent succesfully", props})
-            
-        });
-    });
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
 }
-app.get("/", (req, res) => {
-    sendEmail()
-    .then((response) => res.send(response.message))
-    .catch((error) => res.status(500).send(error.message))
-})
-app.put("/send_email", (req, res) => {
-    sendEmail(req.body)
-    .then((response) => res.send(response.message))
-    .catch((error) => res.status(500).send(error.message))
-})
+
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+
+app.put('/send_email', allowCors(async (req, res) => {
+  const { name, email, number, service, message } = req.body;
+  
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'jwdigitalltd@gmail.com',
+        pass: 'fidvjwawgbyssidd'
+      }
+    });
+
+    const mailOptions = {
+      from: 'jwdigitalltd@gmail.com',
+      to: 'januszwozniak.bth@gmail.com',
+      subject: 'Testing Koding 101 email',
+      text: `Name: ${name}\nEmail: ${email}\nNumber: ${number}\nService: ${service}\nMessage: ${message}`
+    };
+    
+    await transporter.sendMail(mailOptions);
+    
+    res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while sending email' });
+  }
+}));
+
 app.listen(port, () => {
-    console.log(`nodemailerProject is listening at http://localhost:${port}`);
-})
+  console.log(`nodemailerProject is listening at http://localhost:${port}`);
+});
