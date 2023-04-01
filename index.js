7a5aa60503ddf5b4d1bb56ba3a70a8316cb0e84c
Janusz Wozniak <januszwozniak.bth@gmail.com>
2023-04-01 18:57:11 +0100

    vercel config

--- index.js
+++ index.js
@@ -5,18 +5,8 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;;
 
-const whitelist = ["http://localhost:3000"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
+app.use(cors());
+
 
 function sendEmail(props) {
     return new Promise((resolve, reject) => {
