7c730cf83ffcbb21a5cc62565e4b635d5baa4e3a
Janusz Wozniak <januszwozniak.bth@gmail.com>
2023-04-01 17:09:41 +0100

    error

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3001;;
 
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
